1290d185ae13ea6b48ea38a51f17fc938aa6217e
Asm <achan.co2008@gmail.com>
2023-01-28 00:13:12 +0900

    2301最新版へ変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,40 @@
 <template lang="pug">
   section.container.indexBox
     .aboutMeBox
-      p.marginBottom 2022/04/19：サイトテキストを修正しました。現在リニューアル作業中です。
+      p.marginBottom 2023/01/27：サイト情報を最新にしました。
       h2.title.borderBottom.marginBottomHalf
         i.far.fa-grin-wink
         |ABOUT
         span.littleText 私について
       .aboutMeBox-explainBox.marginBottom
         //- p 名前 A M
-        p 趣味でWebサイトを製作し始めてから20年がたちました。実務制作は約10年以上のWebデザイナー兼マークアップエンジニアです。
+        p 趣味でWebサイトを製作し始めてから20年がたちました。実務制作は10年以上のWebデザイナー兼マークアップエンジニアです。
         p Webデザイナーのお仕事は「人の脳内にあるイメージ図を最速で具現化／モックアップ化し、チームや社内で同じ方向を見るためのコミュニケーション」だと思っています。
-        p 制作したサイトやアプリを現状ままで放置をせずに、ユーザーのために継続して改善・提案していくことが得意です。
+        p 制作したサイトやアプリを現状ままで放置をせずに、ユーザーのために
+          span 継続して改善・提案していくことが得意
+          |です。
         p 趣味は英語や韓国語の習得です。
-        p Live2Dでのアニメーション作成やNext.jsを勉強中です。※このサイトはNuxt.jsでできています。
+        |最近は動画制作を勉強していて、サムネイルによる効果検証なども行っています。※このサイトはNuxt.jsでできています。
       dl.borderBox-dl.marginBottom
         dt SKILL
           span.littleText スキル
         dd
           ul
-            li レスポンシブデザイン、Androidアプリデザイン、UXデザイン、material design
-            li HTML5、CSS3、jquery/Javascriptの実装、Nuxt.js
-            li sass(scss)、Gulp4、pug、WordPressテンプレート作成
+            li 0からのバナー制作・LP制作
+            li クライアントやサービスに合わせたイメージ作りや改善提案、新規企画
+            li レスポンシブデザイン、Androidアプリデザイン、UXデザイン
+            li HTML、CSS、jquery/Javascriptの実装、Nuxt.js
+            li sass(scss)、WordPressテンプレート作成
             li GitHub、GitLab
             li SEOを意識したライティング、LPなどの企画・ディレクション/編集業務
       dl.borderBox-dl.marginBottom
         dt 使用可能ソフト
         dd
           ul
-            li Adobe XD、Photoshop CC、Illustrator CC、Zeplin、Figma、
+            li Adobe XD、Photoshop CC、Illustrator CC、Figma、Canva
             li VScode、その他MSオフィスソフト等
-            li Slack、Chat Work、Torello
-            li Premiere pro、Live2D Cubism（習得中）
+            li Slack、Chat Work、Torello、Teams
+            li Premiere pro（習得中）、Live2D Cubism（習得中）
       h3.title-mini.borderBottom-thin.marginBottomHalf
         i.fas.fa-tasks
         |CAREER
@@ -39,10 +43,43 @@
         i.fas.fa-address-card.marginRight
         |過去の仕事
       ul.marginBottom
-        li 勤怠管理システム会社公式HP運用更新など（〜2022現在）
+        li 各大企業の採用領域での集客用バナーやLPなどの作成。(2022/5〜現在)
+        li ディレクターへの技術的・知識的サポート(2022/5〜現在)
+        li 勤怠管理システム会社公式HP運用更新など（〜2022/5）
         li インバウンド向けサイト（英語）のLP制作や既存HPのメンテナンス・更新等（〜2019/11）
         li アンケートの配信や会員向けサイト運営会社のサイトデザイン作成・マークアップ、アプリのUI/UXデザイン・ディレクションなど（〜2019/08）
-      h4.marginBottom 直近の業務内容
+      h4.marginHalfBottom
+        i.fas.fa-address-card.marginRight
+        |直近の制作物（採用関連ページ）
+      ul.marginBottom
+        li
+          a(href="https://recruiting-site.jp/s/toyotasystems/9699" target="_blank" rel="noopener") https://recruiting-site.jp/s/toyotasystems/9699
+        li
+          a(href="https://recruiting-site.jp/s/subaru-move/" target="_blank" rel="noopener") https://recruiting-site.jp/s/subaru-move/
+        li
+          a(href="https://recruiting-site.jp/s/jx-foundry/9831" target="_blank" rel="noopener") https://recruiting-site.jp/s/jx-foundry/9831
+        li
+          a(href="https://recruiting-site.jp/s/sony-semicon-solutions/9832" target="_blank" rel="noopener") https://recruiting-site.jp/s/sony-semicon-solutions/9832
+        li
+          a(href="https://recruiting-site.jp/s/hazurechi-community/" target="_blank" rel="noopener") https://recruiting-site.jp/s/hazurechi-community/
+        li
+          a(href="https://recruiting-site.jp/s/sony-semicon-solutions/9836" target="_blank" rel="noopener") https://recruiting-site.jp/s/sony-semicon-solutions/9836
+      ol.workedBanners.marginBottom
+        li
+          img(src="@/static/img/banner1.png")
+        li
+          img(src="@/static/img/banner2.jpeg")
+          br
+          |↑LINEリッチメニュー
+        li
+          img(src="@/static/img/banner3.png")
+        li
+          img(src="@/static/img/banner4.png")
+          br
+          |↑Facebook/Instagramなど用のバナー
+      h4.marginBottom
+        i.fas.fa-address-card.marginRight
+        |過去の業務内容
         nuxt-link(nuxt-link :to="`/gallery/`").littleText
           i.fas.fa-angle-double-right
           |一覧で見る
@@ -61,16 +98,16 @@
       h4.marginBottomHalf
         i.fas.fa-address-card.marginRight
         |過去の経歴
-      span.littleText 実務9年/個人制作を含むとホームページ制作歴20年
+      span.littleText 実務10年以上/個人制作を含むとホームページ制作歴20年
       ul.marginBottom
         li バンタンデザイン研究所グラフィックデザイン専攻本科卒
         li 玩具系デザイン会社(B to C)　WEBデザイン/コーダー
         li ゲーム会社(B to C)　WEBデザイン/コーダー
         li 物流会社(B to B)　WEBデザイン/コーダー/進行管理
         li ECサイト系制作会社(B to B to C)　WEBデザイン/コーダー/進行管理
-        //- li アンケート系制作会社(B to B or C)　
-        //-   br
-        //-   |WEB・UI・UXデザイン/マークアップ/フロントエンドエンジニア/進行管理/ディレクション
+        li アンケート系制作会社(B to B or C)　
+          br
+          |WEB・UI・UXデザイン/マークアップ/フロントエンドエンジニア/進行管理/ディレクション
       hr(noshade)
       .textBox
         p 中学生のころよりHP制作を開始・独学でHTMLを学び、専門学校ではデザインの基礎だけでなくPhotoshopやIllustrator、Dreamweaverの使い方を習得し、Flashを独学で習得。
